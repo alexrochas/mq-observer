439ebb07fd0eecb932585d1d0afd6152137ae36f
Alex Rocha <alex.rochas@yahoo.com.br>
2017-07-24 09:25:09 -0300

    feat: message service test with chat

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const messages = require('./messages/messages.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(messages);
 };
